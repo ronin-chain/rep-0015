e1c81d2ac1c6b90963233c742ba50d9b8c4c8a90
Thai Xuan Dang <dang.thai@skymavis.com>
2024-07-08 15:45:05 +0700

    chore: comment for REP15Utils

--- src/REP15Utils.sol
+++ src/REP15Utils.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.18;
 
 library REP15Utils {
   struct Delegation {
@@ -20,18 +20,22 @@ library REP15Utils {
     uint64 readyForDetachmentAt;
   }
 
+  /// @dev Checks if the delegation is active
   function isActive(Delegation storage self) internal view returns (bool) {
     return self.delegated && self.until > block.timestamp;
   }
 
+  /// @dev Checks if the delegation is pending
   function isPending(Delegation storage self) internal view returns (bool) {
     return !self.delegated && self.until > block.timestamp;
   }
 
+  /// @dev Checks if the context is existent
   function isExistent(Context storage self) internal view returns (bool) {
     return self.controller != address(0);
   }
 
+  /// @dev Checks if the token context is requested for detachment
   function hasRequestedForDetachment(TokenContext storage self) internal view returns (bool) {
     return self.readyForDetachmentAt > 0;
   }
